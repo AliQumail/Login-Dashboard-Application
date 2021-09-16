21d3a4318d9140543b4c5f07ba8059ceabd89775
Ali Qumail <aliqumail@gmail.com>
2021-09-16 12:29:39 +0500

    added dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup"
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard';
+
 function App() {
   return (
     <BrowserRouter className="App">
       <Switch>
             <Route exact path='/'component={Login} />
             <Route exact path='/signup' component={Signup} />
+            <Route exact path='/dashboard' component={Dashboard} />
       </Switch>
     
     </BrowserRouter>
