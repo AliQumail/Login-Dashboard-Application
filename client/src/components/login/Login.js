0ea27c17d1479cdb65200d81b32db536940724da
Abuzar <63154720+abuzar7266@users.noreply.github.com>
2021-09-16 15:13:04 -0700

    auth added header axios obj

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -3,8 +3,13 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Axios from "axios";
 import "./Login.css";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import { Redirect } from "react-router";
+import Dashboard from "../dashboard/Dashboard";
 
+const Log  =Axios.create({
+  baseURL:"http://localhost:3001/"
+});
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -16,9 +21,18 @@ export default function Login() {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(username + " + " + password);
-    Axios.post("http://localhost:3001/login", {
+    Log.post("/login", {
       username: username,
       password: password,
+    }).then(res=>{
+      console.log(res);
+        if(res.status===200){
+          localStorage.setItem('token',res.data.token);
+          //you can also store user details on local storage if you are willing to use that much space
+        }
+        else{
+          console.log('Incorrect username or password');//Here you cn set state variable which will indicate true or false value of trials and if trial goes false you can display incorrect or logged in using that state variable
+        }
     })
   }
 
